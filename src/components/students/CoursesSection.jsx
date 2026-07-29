f00dbe272c3c7bfc5b67ba48732d8cf2eda8b1ed
pagarwal884 <pagarwal1145@gmail.com>
2026-07-29 19:21:04 +0530

    Added Course card section code and also add few logics in AppContext page so that it provide data to whole web

--- src/components/students/CoursesSection.jsx
+++ src/components/students/CoursesSection.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CoursesSection = () => {
   return (
-    <div>
-      <h1>Courses Section</h1>
+    <div className='py-16 md:px-40 px-8 text-center'>
+      <h2 className='text-3xl font-medium text-gray-800'>
+        Learn From the Best
+      </h2>
+
+      <p className='mt-4 text-gray-600 max-w-2xl mx-auto'>
+        Explore our carefully curated collection of top-rated courses designed
+        to help you learn, grow, and achieve your goals.
+      </p>
+
+      
+
+      <Link
+        to={'/course-list'}
+        onClick={() => scrollTo(0, 0)}
+        className='inline-block mt-8 text-gray-500 border border-gray-500/30 px-10 py-3 rounded'
+      >
+        Show all courses
+      </Link>
     </div>
   )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
 
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [allCourses, setAllCourses] = useState([])
+
+    // Fetch All Courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
     const value = {
-        
+        currency, allCourses
     }
 
     return (
