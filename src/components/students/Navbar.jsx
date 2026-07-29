8176cafa2eee97b7ff6262796e028182229b6541
pagarwal884 <pagarwal1145@gmail.com>
2026-07-29 14:57:57 +0530

    Added Clerk authentication into my webpage

--- src/components/students/Navbar.jsx
+++ src/components/students/Navbar.jsx
@@ -1,19 +1,24 @@
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
+
+  const { openSignIn } = useClerk()
+  const { user } = useUser()
+  const location = useLocation()
 
   const isCourseListPage = location.pathname.includes('/course-list')
 
   return (
-    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : "bg-cyan-100/70"}`}>
+    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
 
       <img src={assets.logo} alt="" className='w-28 lg:w-32 cursor-pointer' />
 
+      {/* Desktop */}
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
 
-        <div className='item-center flex gap-5'>
-
+        <div className='flex items-center gap-5'>
           <button>Become Educator</button>
 
           <span>|</span>
@@ -21,29 +26,41 @@ const Navbar = () => {
           <Link to='/my-enrollments'>
             My Enrollments
           </Link>
-
         </div>
 
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>
-          Create Account
-        </button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={() => openSignIn()}
+            className='bg-blue-600 text-white px-5 py-2 rounded-full'
+          >
+            Create Account
+          </button>
+        )}
 
       </div>
 
-      {/* For Phone Screen */}
+      {/* Mobile */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
 
-        <div className=''>
+        <div>
           <button>Become Educator</button>
 
-          | <Link to='/my-enrollments'>
+          <span>|</span>
+
+          <Link to='/my-enrollments'>
             My Enrollments
           </Link>
         </div>
 
-        <button>
-          <img src={assets.user_icon} alt="" />
-        </button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button onClick={() => openSignIn()}>
+            <img src={assets.user_icon} alt="" />
+          </button>
+        )}
 
       </div>
 
