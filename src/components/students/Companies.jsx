5b944853efc3ef653e1578e25a28372a98c865ae
pagarwal884 <pagarwal1145@gmail.com>
2026-07-29 18:49:52 +0530

    added Search section and also companies section too

--- src/components/students/Companies.jsx
+++ src/components/students/Companies.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Companies = () => {
   return (
     <div>
-      <h1>Companies</h1>
+      <p>Trusted by learners from</p>
+      <div className='flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5'>
+        <img src={assets.microsoft_logo} alt="" className="w-20 md:w-28" />
+        <img src={assets.walmart_logo} alt="" className="w-20 md:w-28" />
+        <img src={assets.accenture_logo} alt="" className="w-20 md:w-28" />
+        <img src={assets.paypal_logo} alt="" className="w-20 md:w-28" />
+        <img src={assets.adobe_logo} alt="" className="w-20 md:w-28" />
+        </div>
     </div>
   )
 }
--- src/components/students/SearchBar.jsx
+++ src/components/students/SearchBar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
-const SearchBar = () => {
+const SearchBar = ({data}) => {
+  const navigate = useNavigate()
+  const [input, setInput] = useState(data ? data : '')
+
+  const onSearchHandler = (e) => {
+    e.preventDefault()
+    navigate('/course-list/' + input)
+  }
   return (
-    <form className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded">
+    <form onSubmit={onSearchHandler} className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded">
       <img
         src={assets.search_icon}
         alt=""
@@ -11,6 +19,8 @@ const SearchBar = () => {
       />
 
       <input
+      onChange={e => setInput(e.target.value)}
+      value={input}
         type="text"
         placeholder="Search for Courses"
         className="w-full h-full outline-none text-gray-500/80"
